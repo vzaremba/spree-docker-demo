5d837aa5fae7820b79f98ccd8e5af3e5d86b69d6
Viktor Zaremba <c_vzaremba@groupon.com>
2020-04-22 14:14:22 -0500

    Update credentials

--- config/deploy/production.sh
+++ config/deploy/production.sh
@@ -8,7 +8,7 @@ deploy \
   --aws-access-key "$AWS_ACCESS_KEY_ID" \
   --aws-secret-key "$AWS_SECRET_ACCESS_KEY" \
   --region 'us-east-1' \
-  --repo "$YOUR_ECR_ID.dkr.ecr.us-east-1.amazonaws.com/spreeproject/server_app" \
+  --repo "384951611022.dkr.ecr.us-east-1.amazonaws.com/spreeproject/server_app" \
   --cluster 'spreeproject-production' \
   --service 'spreeproject'
   --task 'spreeproject-production'
@@ -19,7 +19,7 @@ deploy \
   --aws-access-key "$AWS_ACCESS_KEY_ID" \
   --aws-secret-key "$AWS_SECRET_ACCESS_KEY" \
   --region 'us-east-1' \
-  --repo "$YOUR_ECR_ID.dkr.ecr.us-east-1.amazonaws.com/spreeproject/server_app" \
+  --repo "384951611022.dkr.ecr.us-east-1.amazonaws.com/spreeproject/server_app" \
   --cluster 'spreeproject-production' \
   --service 'spreeproject-worker'
   --task 'spreeproject-production-worker'
--- config/deploy/staging.sh
+++ config/deploy/staging.sh
@@ -7,7 +7,7 @@ deploy \
   --aws-access-key "$AWS_ACCESS_KEY_ID" \
   --aws-secret-key "$AWS_SECRET_ACCESS_KEY" \
   --region 'us-east-1' \
-  --repo "$YOUR_ECR_ID.dkr.ecr.us-east-1.amazonaws.com/spreeproject/server_app" \
+  --repo "384951611022.dkr.ecr.us-east-1.amazonaws.com/spreeproject/server_app" \
   --cluster 'spreeproject-staging' \
   --service 'spreeproject'
   --task 'spreeproject-staging'
